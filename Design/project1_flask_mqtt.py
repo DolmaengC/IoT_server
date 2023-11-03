e5a07310da605dec11d848d6bebe1ea47b6d18af
DolmaengC <21900764@handong.ac.kr>
2023-11-03 15:32:32 +0900

    first commit

---
+++ Design/project1_flask_mqtt.py
@@ -0,0 +1,199 @@
+from flask import Flask, render_template
+from flask_mqtt import Mqtt
+import time # library for time delay
+
+app = Flask(__name__)
+
+app.config['MQTT_BROKER_URL'] = 'sweetdream.iptime.org'
+app.config['MQTT_BROKER_PORT'] = 1883 
+app.config['MQTT_USERNAME'] = 'iot'
+app.config['MQTT_PASSWORD'] = 'csee1414'
+'''
+app.config['MQTT_BROKER_URL'] = '192.168.0.123'
+app.config['MQTT_BROKER_PORT'] = 1883 
+app.config['MQTT_USERNAME'] = 'junhyeok'
+app.config['MQTT_PASSWORD'] = 'jun102030'
+'''
+
+mqtt = Mqtt(app)
+
+# 21900764
+pub_topic_21900764 = 'iot/21900764' # iot/{student id}
+sub_topic_dht22_21900764 = 'iot/21900764/dht22'
+sub_topic_dht22_h_21900764 = 'iot/21900764/dht22_h'
+sub_topic_dht22_t_21900764 = 'iot/21900764/dht22_t'
+sub_topic_cds_21900764 = 'iot/21900764/cds'
+# 22100768
+pub_topic_22100768 = 'iot/22100768' # iot/{student id}
+sub_topic_dht22_22100768 = 'iot/22100768/dht22'
+sub_topic_dht22_h_22100768 = 'iot/22100768/dht22_h'
+sub_topic_dht22_t_22100768 = 'iot/22100768/dht22_t'
+sub_topic_cds_22100768 = 'iot/22100768/cds'
+#21800677
+pub_topic_21800677 = 'iot/21800677' # iot/{student id}
+sub_topic_dht22_21800677 = 'iot/21800677/dht22'
+sub_topic_dht22_h_21800677 = 'iot/21800677/dht22_h'
+sub_topic_dht22_t_21800677 = 'iot/21800677/dht22_t'
+sub_topic_cds_21800677 = 'iot/21800677/cds'
+
+# global varuable for messge payload
+mqtt_message = ''
+
+json_data = {
+    "temperature_21900764" : '',
+    "humidity_21900764" : '',
+    "light_intensity_21900764" : '',
+    "temperature_22100768" : '',
+    "humidity_22100768" : '',
+    "light_intensity_22100768" : '', 
+    "temperature_21800677" : '', 
+    "humidity_21800677" : '',
+    "light_intensity_21800677" : '' 
+}
+
+print('@@ Use URL:/iot/21900764/{led,ledon,ledoff,dht22,cds}')
+
+@app.route('/iot/21900764/')
+def my_home():
+    return render_template('index_team_project1.html', json_data=json_data) 
+
+@app.route('/')
+def home():
+	return render_template('index_team_project1.html', json_data=json_data) 
+
+# cmd = {led, ledon, ledoff, usbled, usbledon, usbledoff}
+@app.route('/iot/21900764/<cmd>') 
+def get_command_21900764(cmd):
+	global mqtt_message
+    
+	if cmd == 'led':
+		mqtt.publish(pub_topic_21900764, 'led')
+		return render_template('index_team_project1.html', json_data=json_data) 
+	elif cmd == 'ledon':
+		mqtt.publish(pub_topic_21900764, 'ledon')
+		return render_template('index_team_project1.html', json_data=json_data) 
+	elif cmd == 'ledoff':
+		mqtt.publish(pub_topic_21900764, 'ledoff')
+		return render_template('index_team_project1.html', json_data=json_data) 
+
+	elif cmd == 'usbled':
+		mqtt.publish(pub_topic_21900764, 'usbled')
+		return render_template('index_team_project1.html', json_data=json_data) 
+	elif cmd == 'usbledon':
+		mqtt.publish(pub_topic_21900764, 'usbledon')
+		return render_template('index_team_project1.html', json_data=json_data) 
+	elif cmd == 'usbledoff':
+		mqtt.publish(pub_topic_21900764, 'usbledoff')
+		return render_template('index_team_project1.html', json_data=json_data) 
+
+# cmd = {led, ledon, ledoff, usbled, usbledon, usbledoff}
+@app.route('/iot/22100768/<cmd>') 
+def get_command_22100768(cmd):
+	global mqtt_message
+    
+	if cmd == 'led':
+		mqtt.publish(pub_topic_22100768, 'led')
+		return render_template('index_team_project1.html', json_data=json_data) 
+	elif cmd == 'ledon':
+		mqtt.publish(pub_topic_22100768, 'ledon')
+		return render_template('index_team_project1.html', json_data=json_data) 
+	elif cmd == 'ledoff':
+		mqtt.publish(pub_topic_22100768, 'ledoff')
+		return render_template('index_team_project1.html', json_data=json_data) 
+
+	elif cmd == 'usbled':
+		mqtt.publish(pub_topic_22100768, 'usbled')
+		return render_template('index_team_project1.html', json_data=json_data) 
+	elif cmd == 'usbledon':
+		mqtt.publish(pub_topic_22100768, 'usbledon')
+		return render_template('index_team_project1.html', json_data=json_data) 
+	elif cmd == 'usbledoff':
+		mqtt.publish(pub_topic_22100768, 'usbledoff')
+		return render_template('index_team_project1.html', json_data=json_data) 
+
+# cmd = {led, ledon, ledoff, usbled, usbledon, usbledoff}
+@app.route('/iot/21800677/<cmd>') 
+def get_command_21800677(cmd):
+	global mqtt_message
+    
+	if cmd == 'led':
+		mqtt.publish(pub_topic_21800677, 'led')
+		return render_template('index_team_project1.html', json_data=json_data) 
+	elif cmd == 'ledon':
+		mqtt.publish(pub_topic_21800677, 'ledon')
+		return render_template('index_team_project1.html', json_data=json_data) 
+	elif cmd == 'ledoff':
+		mqtt.publish(pub_topic_21800677, 'ledoff')
+		return render_template('index_team_project1.html', json_data=json_data) 
+
+	elif cmd == 'usbled':
+		mqtt.publish(pub_topic_21800677, 'usbled')
+		return render_template('index_team_project1.html', json_data=json_data) 
+	elif cmd == 'usbledon':
+		mqtt.publish(pub_topic_21800677, 'usbledon')
+		return render_template('index_team_project1.html', json_data=json_data) 
+	elif cmd == 'usbledoff':
+		mqtt.publish(pub_topic_21800677, 'usbledoff')
+		return render_template('index_team_project1.html', json_data=json_data) 
+
+
+# When mqtt is connected, subscribe to following topics
+@mqtt.on_connect()
+def handle_connect(client, userdata, flags, rc):
+    mqtt.subscribe(sub_topic_dht22_21900764)
+    mqtt.subscribe(sub_topic_dht22_t_21900764)
+    mqtt.subscribe(sub_topic_dht22_h_21900764)
+    mqtt.subscribe(sub_topic_cds_21900764)
+
+    mqtt.subscribe(pub_topic_22100768)
+    mqtt.subscribe(sub_topic_dht22_22100768)
+    mqtt.subscribe(sub_topic_dht22_t_22100768)
+    mqtt.subscribe(sub_topic_dht22_h_22100768)
+    mqtt.subscribe(sub_topic_cds_22100768)
+    
+    mqtt.subscribe(pub_topic_21800677)
+    mqtt.subscribe(sub_topic_dht22_21800677)
+    mqtt.subscribe(sub_topic_dht22_t_21800677)
+    mqtt.subscribe(sub_topic_dht22_h_21800677)
+    mqtt.subscribe(sub_topic_cds_21800677)
+
+# When mqtt receives message from subscribed topic
+@mqtt.on_message()
+def handle_mqtt_message(client, userdata, message):
+    global mqtt_message
+    
+    global json_data
+
+    topic = message.topic
+    payload = message.payload
+    payload = payload.decode('utf-8')
+    topic = str(message.topic)
+    payload = str(message.payload)[2:-1]
+
+    if topic == sub_topic_dht22_h_21900764:
+        json_data['humidity_21900764'] = payload 
+    elif topic == sub_topic_dht22_t_21900764:
+        json_data['temperature_21900764'] = payload 
+    elif topic == sub_topic_cds_21900764:
+        json_data['light_intensity_21900764'] = payload 
+    
+    elif topic == sub_topic_dht22_h_22100768:
+        json_data['humidity_22100768'] = payload 
+    elif topic == sub_topic_dht22_t_22100768:
+        json_data['temperature_22100768'] = payload 
+    elif topic == sub_topic_cds_22100768:
+        json_data['light_intensity_22100768'] = payload 
+    
+    elif topic == sub_topic_dht22_h_21800677:
+        json_data['humidity_21800677'] = payload 
+    elif topic == sub_topic_dht22_t_21800677:
+        json_data['temperature_21800677'] = payload 
+    elif topic == sub_topic_cds_21800677:
+        json_data['light_intensity_21800677'] = payload 
+    
+
+    mqtt_message = payload
+    print("Topic: " + topic + "	message: " + mqtt_message)
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=80, debug=False)
